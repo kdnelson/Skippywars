b23e7d2f204fecca00fc76d087db5af41968fcf4
kdnelson <kris.d.nelson@gmail.com>
2024-08-03 16:03:14 -0700

    Re-align and style product and header pages

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -8,7 +8,6 @@ import { CartService } from '../../service/cart.service';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
   public productList : any ;
   public filterCategory : any
   searchKey:string ="";
@@ -16,25 +15,23 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.api.getProduct()
-    .subscribe(res=>{
-      this.productList = res;
-      this.filterCategory = res;
+    .subscribe(response => {
+      this.productList = response;
+      this.filterCategory = response;
       this.productList.forEach((a:any) => {
-        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
-          a.category ="fashion"
-        }
         Object.assign(a,{quantity:1,total:a.price});
       });
-      console.log(this.productList)
     });
 
     this.cartService.search.subscribe((val:any)=>{
       this.searchKey = val;
     })
   }
+
   addtocart(item: any){
     this.cartService.addtoCart(item);
   }
+
   filter(category:string){
     this.filterCategory = this.productList
     .filter((a:any)=>{
@@ -43,5 +40,4 @@ export class ProductsComponent implements OnInit {
       }
     })
   }
-
 }
